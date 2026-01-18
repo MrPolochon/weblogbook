db1c4ad65a71d82f95b947a6e4a0cdc6e71a49c5
MrPolochon <bonnofpf@gmail.com>
2026-01-18 09:29:13 -1000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: object }[]) {
           cookiesToSet.forEach(({ name, value }) =>
             response.cookies.set(name, value, { path: '/' })
           );
